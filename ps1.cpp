51cf157f41931fce9020f4a91e375e69071dc41b
Joseph S. Pacheco-Corwin <hammersamatom@gmail.com>
2024-04-25 17:34:32 -0400

    I mean, it works now
    Added runCommand to replace std::system, which required use of temporary
    files and IO streams to bring the file back in. Thanks Block.
    Uses an input string as a command, uses a FILE* to take it's output and
    forwards the FILE* to a lambda in the form of a std::string with a
    length of the buffer size
    
    Added Block's codepointToUTF8 function, since working with UTF-8 in any
    other way is tiring.

--- ps1.cpp
+++ ps1.cpp
@@ -2,15 +2,67 @@
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
-#include <iterator>
 #include <string>
-#include <fstream>
-#include <filesystem>
+#include <functional>
+#include <iostream>
 
 #include <fmt/core.h>
 #include <fmt/color.h>
 
 
+// Stole the code from my personal debugger LOL
+#ifdef _WIN32
+    #define popen _popen //Windows has _popen, but not just popen. Exact same signature and function
+    #define pclose _pclose //Same reason
+#endif
+
+int runCommand(const std::string& command, std::function<void(const std::string&)> callback, size_t outputBufferSize) {
+    FILE* actionOutput = popen(command.c_str(), "r");
+    if (actionOutput == nullptr) {
+        throw std::runtime_error("Failed to run command");
+    }
+    size_t bytesRead = 0;
+    std::vector<char> chunk(outputBufferSize);
+    while ((bytesRead = fread(chunk.data(), sizeof(char), chunk.size(), actionOutput)) > 0) {
+        callback(std::string(chunk.data(), bytesRead));
+    }
+    if (ferror(actionOutput) != 0) {
+        //Error occured. Not normal EOF
+        throw std::system_error(std::make_error_code((std::errc)errno));
+    }
+    if (feof(actionOutput) == 0) {
+        //EOF should be set, but isn't
+        throw std::runtime_error("Unexpected EOF when reading command output");
+    }
+    return pclose(actionOutput);
+}
+
+std::string codepointToUTF8(uint32_t codepoint) {
+    std::string utf8;
+    if (codepoint < 0x80) {
+        //One byte only. Cast directly to uint8_t (safe to do here)
+        utf8.push_back(codepoint);
+    } else if (codepoint < 0x0800) {
+        //Two bytes
+        utf8.push_back(0xC0 | (codepoint >> 6 & 0x1F));
+        utf8.push_back(0x80 | (codepoint >> 0 & 0x3F));
+    } else if (codepoint < 0x10000) {
+        //Three
+        utf8.push_back(0xE0 | (codepoint >> 12 & 0x0F));
+        utf8.push_back(0x80 | (codepoint >> 6 & 0x3F));
+        utf8.push_back(0x80 | (codepoint >> 0 & 0x3F));
+    } else if (codepoint < 0x110000) {
+        //Four
+        utf8.push_back(0xF0 | (codepoint >> 18 & 0x1F));
+        utf8.push_back(0x80 | (codepoint >> 12 & 0x3F));
+        utf8.push_back(0x80 | (codepoint >> 6 & 0x3F));
+        utf8.push_back(0x80 | (codepoint >> 0 & 0x3F));
+    } else {
+        //Cannot be encoded with UTF-8
+        throw std::runtime_error("Invalid codepoint for UTF-8 encoding");
+    }
+    return utf8;
+}
 
 enum DIRECTION {
 	FRONT,
@@ -22,6 +74,9 @@ std::size_t strip_char(std::string& input, char delimiter, DIRECTION dir = BACK)
 {
 	std::size_t count = 0;
 	
+	if (input.empty())
+		return count;
+
 	switch (dir)
 	{
 		case BACK:
@@ -45,33 +100,18 @@ std::size_t strip_char(std::string& input, char delimiter, DIRECTION dir = BACK)
 
 std::string get_git_branch()
 {
-	int retval = std::system("git rev-parse --abbrev-ref HEAD 2> /dev/null > /tmp/git.tmp");
-	//if (retval)
-	//{
-	//	fmt::print("Command returned with code {}\n", retval);
-	//	exit(1);
-	//}
-
-	std::ifstream tmpfile_handler = std::ifstream("/tmp/git.tmp");
-	if (!tmpfile_handler.is_open())
-	{
-		fmt::print("Failed to open file\n");
-		exit(2);
-	}
-
-	std::string contents((std::istreambuf_iterator<char>(tmpfile_handler)), (std::istreambuf_iterator<char>()));
-
-	//return "No Branch";
-
-	strip_char(contents, '\n');
-	return contents;
+	std::string branch;
+	int retval = runCommand("git rev-parse --abbrev-ref HEAD 2> /dev/null", [&branch](const std::string& buf) -> void{ branch += buf; }, 128);
+	strip_char(branch, '\n');
+	return branch;
 }
 
 std::string get_current_dir()
 {
 	std::string path = "";
 	
-	try {
+	try
+	{
 		path = std::getenv("PWD");
 	}
 	catch (std::exception& e)
@@ -84,17 +124,20 @@ std::string get_current_dir()
 
 std::string get_current_user()
 {
-	std::system("whoami 2> /dev/null > /tmp/usr.tmp");
-	std::ifstream name_handler = std::ifstream("/tmp/usr.tmp");
-	std::string name((std::istreambuf_iterator<char>(name_handler)), (std::istreambuf_iterator<char>()));
+	std::string name;
+	runCommand("whoami 2> /dev/null", [&name](const std::string& buf) -> void{ name += buf; }, 128);
 	strip_char(name, '\n');
 	return name;
 }
 
 int main(int argc, char* argv[])
 {
-	fmt::print("{}", fmt::styled(fmt::format(" {} ", get_current_user()), fmt::bg(fmt::color::dark_cyan) | fmt::fg(fmt::color::white)));
-	fmt::print("{}", fmt::styled(fmt::format(" {} ", get_current_dir()), fmt::bg(fmt::color::slate_gray) | fmt::fg(fmt::color::white)));	
-	fmt::print("{}", fmt::styled(fmt::format("  {} ", get_git_branch()), fmt::bg(fmt::color::purple) | fmt::fg(fmt::color::white)));
+	fmt::print(fmt::bg(fmt::color::dark_cyan) | fmt::fg(fmt::color::white), " {} ", get_current_user());
+	fmt::print(fmt::bg(fmt::color::black) | fmt::fg(fmt::color::dark_cyan), "{}", codepointToUTF8(0xe0b4));
+	fmt::print(fmt::bg(fmt::color::black) | fmt::fg(fmt::color::white), " {} ", get_current_dir());	
+	fmt::print(fmt::bg(fmt::color::brown) | fmt::fg(fmt::color::black), "{}", codepointToUTF8(0xe0b4));
+	fmt::print(fmt::bg(fmt::color::brown) | fmt::fg(fmt::color::white), "  {} ", get_git_branch());
+	fmt::print(fmt::fg(fmt::color::brown), "{} ", codepointToUTF8(0xe0b4));
+
 	return 0;
 }
